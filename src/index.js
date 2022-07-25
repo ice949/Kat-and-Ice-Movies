3654e9aa4280cf06eee969d6951beed3c76402d0
ice949 <justineimasiku21@gmail.com>
2022-07-26 00:54:00 +0200

    Add header to the home page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-// import './style.css';
+import './style.css';
 // import component from './modules/component.js';
 
-// window.onload = component();
 
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
